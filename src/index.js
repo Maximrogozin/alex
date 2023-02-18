48e215b0a4fa833d62c4dc857686b493fa39ad15
Alexey <alexey_prog95@mail.ru>
2023-02-18 16:11:42 +0300

    author version

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import "bootstrap/dist/css/bootstrap.css";
 import reportWebVitals from "./reportWebVitals";
-import App from "./app";
+import "bootstrap/dist/css/bootstrap.css";
+import App from "./app/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
